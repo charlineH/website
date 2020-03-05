d9990dc0e3b4966b8efe00be5072546bd5137004
Charline HUMEAU <c.humeau@groupeonepoint.com>
2020-03-05 20:12:51 +0100

    :sparkles: :lipstick: Add news component
    Add route path enum

---
+++ website/src/app/enum/routes.enum.ts
@@ -0,0 +1,9 @@
+export enum RoutesPath {
+  SHIATSU = 'shiatsu',
+  HOME = '',
+  MASSAGE = 'massage',
+  TAICHI = 'taichi',
+  A_PROPOS = 'a-propos',
+  GALLERY = 'gallerie',
+  ENTREPRISE = 'entreprise'
+}
