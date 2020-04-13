48b2fbcb6f1973cd7ebc88b050548e5b580eeb0e
Charline HUMEAU <c.humeau@groupeonepoint.com>
2020-04-13 22:47:57 +0200

    :spakles: Add tachi page UI

--- website/src/app/pages/massage/massage.component.ts
+++ website/src/app/pages/massage/massage.component.ts
@@ -1,19 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked, Component, OnInit } from '@angular/core';
 import { RoutesPath } from '../../enum/routes.enum';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-massage',
   templateUrl: './massage.component.html',
   styleUrls: ['./massage.component.scss']
 })
-export class MassageComponent implements OnInit {
+export class MassageComponent implements OnInit, AfterViewChecked {
 
   readonly page = RoutesPath.MASSAGE;
 
-  constructor() {
+  constructor(private readonly route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewChecked(): void {
+    this.scrollToAnchor(this.route.snapshot.queryParams.anchor);
+  }
+
+  scrollToAnchor(anchor: string): void {
+    try {
+      if (anchor) {
+        document.querySelector(`#${ anchor }`).scrollIntoView();
+      }
+    } catch (e) {
+      console.error('Scoll impossible', e);
+    }
   }
 
 }
