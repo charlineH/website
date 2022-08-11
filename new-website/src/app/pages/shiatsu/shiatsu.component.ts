5929e0bbd788a2199d4b6cb344ba5f2b41f57040
Marie-Laure Humeau <mlh@humoe.fr>
2022-08-11 16:59:38 +0200

    shiatsu : html scss ts images

--- new-website/src/app/pages/shiatsu/shiatsu.component.ts
+++ new-website/src/app/pages/shiatsu/shiatsu.component.ts
@@ -1,16 +1,36 @@
-import { Component } from '@angular/core';
-import { RoutesPath } from '../../enum/routes.enum';
-import { HasTitle } from '../../model/has-title.model';
+import { Component, ElementRef } from "@angular/core";
+import { RoutesPath } from "../../enum/routes.enum";
+import { HasTitle } from "../../model/has-title.model";
 
 @Component({
-  selector: 'app-shiatsu',
-  templateUrl: './shiatsu.component.html',
-  styleUrls: ['./shiatsu.component.scss']
+  selector: "app-shiatsu",
+  templateUrl: "./shiatsu.component.html",
+  styleUrls: ["./shiatsu.component.scss"],
 })
 export class ShiatsuComponent implements HasTitle {
-
   readonly page = RoutesPath.SHIATSU;
 
-  title = 'Shiatsu au Mans (72) | Humoe';
+  title = "Shiatsu au Mans (72) | Humoe";
+  goToChapter1() {
+    this.scrollToAnchor("bienfaits");
+  }
+  constructor(private el: ElementRef) {}
 
+  goToChapter2() {
+    this.scrollToAnchor("origine");
+  }
+
+  goToChapter3() {
+    this.scrollToAnchor("deroulement");
+  }
+  private scrollToAnchor(anchor: string): void {
+    try {
+      if (anchor) {
+        const toScrollEl = this.el.nativeElement.querySelector(`#${anchor}`);
+        toScrollEl.scrollIntoView();
+      }
+    } catch (e) {
+      console.error("Scoll impossible", e);
+    }
+  }
 }
